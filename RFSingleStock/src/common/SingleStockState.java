cbfbaa57c2ed9ed6651dde19f9bc2040c5c5414a
tranh <tranh@DESKTOP-G1ROG4L.T-mobile.com>
2018-01-30 14:52:58 -0500

    in progress

--- RFSingleStock/src/common/SingleStockState.java
+++ RFSingleStock/src/common/SingleStockState.java
@@ -1,5 +1,10 @@
 package common;
 
 public class SingleStockState {
-
+	private int holding;
+	private double price;
+	public SingleStockState(int holding, double price) {
+		this.holding = holding;
+		this.price = price;
+	}
 }
--- RFSingleStock/src/environment/SingleStockTrader.java
+++ RFSingleStock/src/environment/SingleStockTrader.java
@@ -11,14 +11,36 @@ public class SingleStockTrader {
 	
 	private int holding;
 	private double lastTransactionCost;
+	private double lastSeenPrice;
 	private double wealth;
 	private int stepCount;
 	
 	private double reward;
 	private SingleStockState state;
 	
+	public SingleStockTrader(String name, double utility, Learner learner, SingleStockExchange exchange) {
+		assert utility >= 0;
+		this.name = name;
+		this.utility = utility;
+		this.learner = learner;
+		this.exchange = exchange;
+		
+		this.holding = 0;
+		this.wealth = 0;
+		this.stepCount = -1;
+		
+		this.exchange.registerTrader(this);
+	}
+	
 	public void getNotified(double price) {
-		// TODO Auto-generated method stub
+		if (stepCount == -1) {
+			// at initialization
+			lastSeenPrice = price;
+			stepCount += 1;
+			state = new SingleStockState(holding, lastSeenPrice);
+			return;
+		}
+		
 		
 	}
 	public void resetEpisode() {
