d2c5b8d92a9b60e0a67bdfb3e54529e9eb1045f7
tranh <tranh@172.16.206.30>
2018-02-09 16:49:50 -0500

    write sharpe output

--- RFSingleStock/src/environment/SingleStockMain.java
+++ RFSingleStock/src/environment/SingleStockMain.java
@@ -1,9 +1,13 @@
 package environment;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
+import org.netlib.util.doubleW;
 
 import common.AssetConfig;
 import learner.Learner;
@@ -16,6 +20,8 @@ import stock.Stock;
 public class SingleStockMain {
 	
 	private static final String rfsarsa = "RF Sarsa", tabularq = "Tabular Q", tabularsarsa = "Tabular Sarsa";
+	private static final String[] traderNames = {rfsarsa, tabularq, tabularsarsa};
+	private static final int ntrain = 6000, ntest = 1000, ntrials = 500;
 	
 	public static Map<String, Double> trainAndTest(SingleStockExchange exchange, int ntrain, int ntest) {
 		// run training, then run testing, then return a map of trader's name to its Sharpe ratio during ntest
@@ -74,7 +80,7 @@ public class SingleStockMain {
 		return sharpe;
 	}
 	
-	public static Map<String, Double> runOneCompleteTrial(int iteration) {
+	public static Map<String, Double> runOneCompleteTrial() {
 		double currentprice = 75, minprice = 0.1, maxprice = 150;
 		double kappa = Math.log(2)/3, mu = Math.log(50), sigma = 0.1; // reversion level is 50
 		Stock stock = new OULogStock(currentprice, minprice, maxprice, kappa, mu, sigma);
@@ -99,12 +105,59 @@ public class SingleStockMain {
 		SingleStockTrader tabularQTrader = new SingleStockTrader(tabularq, utility, tabularQ, exchange);
 		SingleStockTrader tabularSarsaTrader = new SingleStockTrader(tabularsarsa, utility, tabularSarsa, exchange);
 		
-		int ntrain = 6000, ntest = 1000;
 		return trainAndTest(exchange, ntrain, ntest);
 	}
 
+	public static void writeCsv(Map<String, double[]> sharpeRatios) {
+		String filename = "C:\\Users\\tranh\\Documents\\" + ntrain + "train" + ntest + "test";
+		String delimiter = ",";
+		
+		FileWriter fileWriter = null;
+		BufferedWriter bufferedWriter = null;
+		try {
+			fileWriter = new FileWriter(filename);
+			bufferedWriter = new BufferedWriter(fileWriter);
+			
+			bufferedWriter.write(String.join(delimiter, traderNames));
+			bufferedWriter.newLine();
+			
+			for (int i=0; i < ntrials; i++) {
+				for (int k=0; k < traderNames.length; k++) {
+					bufferedWriter.write(String.format("%.4f", sharpeRatios.get(traderNames[k])[i]));
+					if (k < (traderNames.length-1)) {
+						bufferedWriter.write(delimiter);
+					}
+				}
+				if (i < (ntrials-1)) {
+					bufferedWriter.newLine();
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Error in CsvFileWriter");
+			e.printStackTrace();
+		} finally {
+			try {
+				bufferedWriter.close();
+				fileWriter.close();
+			} catch (Exception e2) {
+				System.out.println("Error closing output file");
+				e2.printStackTrace();
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
-
+		Map<String, double[]> sharpeRatios = new HashMap<String, double[]>();
+		for (String name: traderNames) {
+			sharpeRatios.put(name, new double[ntrials]);
+		}
+		for (int i=0; i < ntrials; i++) {
+			Map<String, Double> trialResult = runOneCompleteTrial();
+			for (String name: traderNames) {
+				sharpeRatios.get(name)[i] = trialResult.get(name).doubleValue();
+			}
+		}
+		writeCsv(sharpeRatios);
 	}
 
 }
