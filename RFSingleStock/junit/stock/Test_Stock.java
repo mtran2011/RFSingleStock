05fb9845328c546f9c425e5d5ba011c500059a62
tranh <tranh@DESKTOP-G1ROG4L>
2018-02-21 00:19:34 -0500

    add junit

--- RFSingleStock/junit/stock/Test_Stock.java
+++ RFSingleStock/junit/stock/Test_Stock.java
@@ -2,10 +2,21 @@ package stock;
 
 import junit.framework.TestCase;
 
-import static org.mockito.Mockito.*;
-
 public class Test_Stock extends TestCase {
+	
+	private final double tol = 1e-6;
+	
 	public void test_setPrice() {
-		
+		double price = 30, minprice = 0.1, maxprice = 100;
+		Stock stock = new Stock(price, minprice, maxprice) {
+			@Override
+			public void simulate() {
+				// TODO Auto-generated method stub
+			}
+		};
+		stock.setPrice(maxprice + 0.1);
+		assertEquals(maxprice, stock.getPrice(), tol);
+		stock.setPrice(minprice - tol*2);
+		assertEquals(minprice, stock.getPrice(), tol);
 	}
 }
